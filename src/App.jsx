431cf5e97e339ba0b2d57130083972a898914cf3
pasindu <gallagepasinduhansana@gmail.com>
2024-12-15 17:11:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {}
+return (
+  <div className="App">
+    <h1>Welcome to React!</h1>
+    <p>This is a simple React application.</p>
+  </div>
+);
+export default App;
