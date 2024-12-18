5bb12b554909cd3a018117d9a5cb6684e3f43ac5
sashiksilva <sashiksilva90@gmail.com>
2024-12-18 18:20:40 +0530

    Aboutus

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,24 @@ import Admin from "./Pages/Admin/Admin";
 import Landing from "./Components/Landing";
 import Login from "./Pages/Admin/Login";
 import ContactUs from "./Components/ContactUs";
+import AboutUs from "./Components/AboutUs";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Toaster position="top-center" containerStyle={{ top: 60 }} />
+		// <BrowserRouter>
+		// 	<Toaster position="top-center" containerStyle={{ top: 60 }} />
 
-			<div className="App bg-primarybg text-primarytext">
-				<Routes>
-					<Route path="/" element={<Advertisement />} />
+		// 	<div className="App bg-primarybg text-primarytext">
+		// 		<Routes>
+		// 			<Route path="/" element={<Advertisement />} />
 
-					{/* Admin Routes */}
-					<Route path="/admin" element={<Login />} />
-					<Route path="/admin/dashboard" element={<Admin />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+		// 			{/* Admin Routes */}
+		// 			<Route path="/admin" element={<Login />} />
+		// 			<Route path="/admin/dashboard" element={<Admin />} />
+		// 		</Routes>
+		// 	</div>
 
+		// </BrowserRouter>
+	 <AboutUs/>
+	)}
 export default App;
